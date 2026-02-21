2876a89ef1e1acde0ae0b18e70ac24fe66893334
Gabriela <gabrielaabasilioo@gmail.com>
2026-02-21 16:51:03 -0300

    Adding classes generated from StarUML

---
+++ code/background.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Entity import Entity
+from Entity import Entity
+from Entity import Entity
+from Entity import Entity
+from Entity import Entity
+
+
+class Background(Entity, Entity, Entity, Entity, Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
---
+++ code/entity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.name = None
+        self.surf = None
+        self.rect = None
+
+    def move(self, ):
+        pass
+
+    def update(self, ):
+        pass
+
+    def draw(self, screen):
+        pass
